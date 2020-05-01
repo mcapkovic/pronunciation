164f8cc77582c11416c0a0894554a29152f2c193
Matej Capkovic <mcapkovic@gmail.com>
2020-05-01 21:40:08 +0200

    Refactor app

--- src/App.js
+++ src/App.js
@@ -1,32 +1,11 @@
 import React, { useState } from "react";
-import ReactSwiper from "./components/ReactSwiper";
-import Player from "./components/Player";
 import words from "./words.json";
-import ControlPanel from "./components/ControlPanel";
-import useMultiCard from "./hooks/useMultiCard";
+import MultiCard from "./components/MultiCard";
 
 function App() {
-  const [multiCardState, multiCardActions] = useMultiCard();
-
-  // console.log(state);
   return (
     <div>
-      <Player
-        lesson={words[0]}
-        multiCardActions={multiCardActions}
-        multiCardState={multiCardState}
-      />
-
-      <ReactSwiper
-        examples={words[0].data}
-        multiCardActions={multiCardActions}
-        multiCardState={multiCardState}
-      />
-
-      <ControlPanel
-        multiCardActions={multiCardActions}
-        multiCardState={multiCardState}
-      />
+      <MultiCard lesson={words[0]} />
     </div>
   );
 }
