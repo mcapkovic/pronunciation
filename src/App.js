cd08ba3be7c670e970d3cbde4fa577b550d247a8
Matej Capkovic <mcapkovic@gmail.com>
2020-05-10 18:56:55 +0200

    Add router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
 import React, { useState } from "react";
 import words from "./words.json";
 import MultiCard from "./components/MultiCard";
-import './sass/main.scss'
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import "./sass/main.scss";
+
+function Dashboard() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+
+        <li>
+          <Link to="/multicard">multicard</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
 function App() {
   return (
-    <div>
-      <MultiCard lesson={words[0]} />
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/multicard">
+          <MultiCard lesson={words[0]} />
+        </Route>
+        <Route path="/">
+          <Dashboard />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
